fb0de162412c0f6c57e775b2b549ff21404cb364
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-07-09 10:31:44 +0330

    Update utils.py

--- classes/utils.py
+++ classes/utils.py
@@ -2,8 +2,8 @@ import json
 
 
 class UtilsIR:
-    def save_json_file(self, data):
-        with open("result.json", 'w') as file:
+    def save_json_file(self, data: list | dict, name_file: str = "result"):
+        with open(f"{name_file}.json", 'w') as file:
             json.dump(data, file)
 
     def remove_frequency_words_from_list(self, data: list) -> list:
