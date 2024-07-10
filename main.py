eb37ee4157181a07b460f4be3b8bc9178ce25871
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-07-10 12:30:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from classes.answer import Answer
 
-# text_test = "wedding playing are ability alarm arrival detective elusive glo insane interested italian meanwhile mentally mistreat neighbor news obsessive pregnant pursues receives recognizes recover reignites stellar throughout unstable violence various"
-# text_test = "interested meanwhile wedding unstable"
-
 number_of_row = 40
 answer_q = Answer(number_of_row)
 # answer_q.q1()
