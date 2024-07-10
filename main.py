7c6855f90a37a53fedbbf41ec0e6bbf686166f9c
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-07-10 09:54:11 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from classes.answer import Answer
 # text_test = "wedding playing are ability alarm arrival detective elusive glo insane interested italian meanwhile mentally mistreat neighbor news obsessive pregnant pursues receives recognizes recover reignites stellar throughout unstable violence various"
 # text_test = "interested meanwhile wedding unstable"
 
-number_of_row = 3
+number_of_row = 40
 answer_q = Answer(number_of_row)
-answer_q.q1()
-answer_q.q2()
-answer_q.q3()
-answer_q.q4()
+# answer_q.q1()
+# answer_q.q2()
+# answer_q.q3()
+# answer_q.q4()
+answer_q.q5()
