96e00473e4473b5c59b40f5086a6267ead2db77b
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-07-11 14:47:16 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from classes.answer import Answer
 
-number_of_row = 40
+number_of_row = 500
 answer_q = Answer(number_of_row)
 # answer_q.q1()
 # answer_q.q2()
 # answer_q.q3()
 # answer_q.q4()
-answer_q.q5()
+# answer_q.q5()
+answer_q.q6()
