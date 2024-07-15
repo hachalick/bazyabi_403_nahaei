b3c41fd124022bc1a2d8cf9654d436b7cc9c5759
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-07-15 14:26:14 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from classes.answer import Answer
 
-number_of_row = 50
-answer_q = Answer(number_of_row)
+number_of_row = 3
+filename = "train.csv"
+answer_q = Answer(number_of_row, filename)
 # answer_q.q1()
-# answer_q.q2()
+answer_q.q2()
 # answer_q.q3()
 # answer_q.q4()
 # answer_q.q5()
 # answer_q.q6()
-answer_q.q7()
+# answer_q.q7()
